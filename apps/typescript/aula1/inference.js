501078ec75edf9361ee5f3ea3751c3a8842a6dcc
jean-siqueira <jean.siqueira@outlook.com.br>
2018-12-12 00:20:23 -0200

    first commit

---
+++ apps/typescript/aula1/inference.js
@@ -0,0 +1,17 @@
+//inferencia de tipo
+function showLog(message) {
+    console.log(message);
+}
+showLog('Hello Word ');
+/*
+string
+number
+boolean
+arrays
+Classes
+any
+*/
+var test = ['Hello', 'Test'];
+var isEnable = true;
+var test2;
+//# sourceMappingURL=inference.js.map
---
+++ apps/typescript/aula1/inference.ts
@@ -0,0 +1,19 @@
+//inferencia de tipo
+
+function showLog(message : string){
+    console.log(message);
+}
+
+showLog('Hello Word ');
+/*
+string
+number
+boolean
+arrays
+Classes
+any
+*/
+
+let test: Array<string> = ['Hello', 'Test'];
+let isEnable: boolean = true;
+let test2: ['Hello', 'Test'];
