ce6b869d34b253b01743efe006433b37d07f3430
jean-siqueira <jean.siqueira@outlook.com.br>
2018-12-15 09:41:45 -0200

    component

--- apps/first-angular-cli/src/app/app.component.ts
+++ apps/first-angular-cli/src/app/app.component.ts
@@ -8,11 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Tasks';
 
-  tasks = [];
-
-  task = "";
-
-  add():void{
-    this.tasks.push(this.task);
-  }
 }
