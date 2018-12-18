9140a9f65441b848be67ac55056a238475a4d477
jean-siqueira <jean.siqueira@outlook.com.br>
2018-12-18 00:16:29 -0200

    inicio jwt

---
+++ HelpDesk/src/main/java/com/athena/helpdesk/api/security/jwt/JwtTokenUtil.java
@@ -0,0 +1,66 @@
+package com.athena.helpdesk.api.security.jwt;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+public class JwtTokenUtil implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	static final String CLAIM_KEY_USERNAME = "sub";
+	static final String CLAIM_KEY_CREATED = "created";
+	static final String CLAIM_KEY_EXPIRED = "exp";
+	
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	@Value("${jwt.expiration}")
+	private Long expiration;
+	
+	public String getUsernameFromToken(String token) {
+		String userName;
+		try {
+			final Claims claims = getClaimsFromToken(token);
+			userName = claims.getSubject();
+		}catch(Exception e) {
+			userName = null;
+		}
+		return userName;
+	}
+	
+	public Date getExpirationDateFromToken(String token) {
+		Date expiration;
+		try {
+			final Claims claims = getClaimsFromToken(token);
+			expiration = claims.getExpiration();
+		}catch(Exception e) {
+			expiration = null;
+		}
+		return expiration;
+	}
+	
+	public Claims getClaimsFromToken(String token) {
+		Claims claims;
+		try {
+			claims = Jwts.parser()
+					.setSigningKey(secret)
+					.parseClaimsJws(token)
+					.getBody();
+		}catch (Exception e) {
+			claims = null;
+		}
+		return claims;
+	}
+	
+	private Boolean isTokenExpired(String token) {
+		final Date expiration = getExpirationDateFromToken(token);
+		return expiration.before(new Date());
+	}
+	
+	
+}
